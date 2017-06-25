3e54ee62333df35b311de4d6cba3233902ab2c13
Ricardo Dominguez <richard291190@gmail.com>
2017-06-24 22:08:51 -0300

    Gatillo para iniciar el Solver

---
+++ TP33/src/negocio/Gatillo.java
@@ -0,0 +1,25 @@
+package negocio;
+
+public class Gatillo {
+	
+	private Solver _solver;
+	
+	public Gatillo(Solver s){
+		
+		_solver = s;
+	}
+	
+	public boolean gatillar(String archivo){
+		
+		
+		try{
+			_solver.cargarMaterias(archivo);
+			return true;
+		}
+		catch(IllegalArgumentException e){
+			
+			return false;
+		}
+	}
+
+}
